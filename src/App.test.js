ab3c04819e4355ea49aeed59f4330176133ff6e8
unknown <elidovrich.coding@gmail.com>
2024-04-22 17:35:30 -0700

    fix synthax

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen. ;
+  const linkElement = screen.getByText("error");
   expect(linkElement).toBeInTheDocument();
 });
